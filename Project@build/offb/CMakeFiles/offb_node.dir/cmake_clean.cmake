baf93dbbd2918021f10967bec1767446196fc853
vladyslav <vladyslav@ubuntu>
2019-10-20 01:56:00 -0700

    initial commit

---
+++ Project@build/offb/CMakeFiles/offb_node.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/offb_node.dir/src/offb_node.cpp.o"
+  "CMakeFiles/offb_node.dir/sources/PracticalSocket.cpp.o"
+  "/home/vladyslav/catkin_ws/devel/lib/offb/offb_node.pdb"
+  "/home/vladyslav/catkin_ws/devel/lib/offb/offb_node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/offb_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Project@build/ros_dronemap/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/vladyslav/catkin_ws/src/ros_dronemap
+# Build directory: /home/vladyslav/catkin_ws/build/ros_dronemap
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
